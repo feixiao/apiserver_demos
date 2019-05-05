b1b02b35f413ce3195c1068ecde9ab4fcb83cf53
feixiao <feixiao2020@sina.com>
2019-05-05 18:55:48 +0800

    注解demo01

--- demo01/router/router.go
+++ demo01/router/router.go
@@ -14,8 +14,11 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	// Middlewares.
 	// 在处理某些请求时可能因为程序 bug 或者其他异常情况导致程序 panic，这时候为了不影响下一次请求的调用，需要通过 gin.Recovery()来恢复 API 服务器
 	g.Use(gin.Recovery())
+	// 强制浏览器不使用缓存
 	g.Use(middleware.NoCache)
+	// 浏览器跨域 OPTIONS 请求设置
 	g.Use(middleware.Options)
+	// 一些安全设置
 	g.Use(middleware.Secure)
 	g.Use(mw...)
 	// 404 Handler.
